037aac062b46fdb6b6a454f7562d675534102207
andyh0913 <andyh0913@gmail.com>
2019-05-30 11:53:55 +0800

    frontend and backend video stream connected

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import base64
 from flask import Flask, request, send_from_directory
 from flask_socketio import SocketIO, emit
 import requests
@@ -6,14 +7,24 @@ from flask_cors import CORS
 
 app = Flask(__name__, static_folder='build')
 CORS(app)
-io = SocketIO(app)
+socket = SocketIO(app)
 
 img_file = None
+img_string = ''
+
+@socket.on('connect')
+def on_connect():
+	print ("User connected!")
 
 @app.route("/api", methods=["POST"])
 def decode():
 	global img_file
 	img_file = request.files['webcam']
+	# img_string = base64.b64encode(img_file.read()) # base64 is larger
+	img_string = img_file.read()
+	# print (img_string)
+	# print (type(img_string))
+	socket.emit('get_image', {'image_data': img_string})
 	return "POST received!"
 
 @app.route("/", defaults={'path': ''})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Webcam from './Webcam.js'
 import './App.css';
+
+const io = require('socket.io-client')('http://localhost:5000')
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam socket={io}></Webcam>
     </div>
   );
 }
